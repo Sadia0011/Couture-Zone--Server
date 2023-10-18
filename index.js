a63625ee831f7c6e2250ec40efca317b9217a593
sadia sultana <sadiamoon2019@gmail.com>
2023-10-18 22:27:56 +0600

    added create operation for brand

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 const {
     MongoClient,
-    ServerApiVersion
+    ServerApiVersion,
+    ObjectId
 } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -43,6 +44,25 @@ async function run() {
         app.get("/products", async (req, res) => {
             const cursor = fashionCollection.find();
             const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get("/products/:brand", async (req, res) => {
+            const brand = req.params.brand;
+            const query = {
+                brand: brand
+            }
+            const result = await fashionCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await fashionCollection.findOne(query)
             res.send(result)
         })
 
